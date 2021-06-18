f09aaa08ae62f5672e85200aeec4f375f5392f51
Steven Chang <steven.chang@btn.group>
2021-06-18 18:32:34 +1000

    [BG-323] String it all back apart from change_admin (#2)

--- src/contract.rs
+++ src/contract.rs
@@ -2,7 +2,7 @@ use crate::msg::{BalanceResponse, ConfigResponse, HandleMsg, InitMsg, QueryMsg};
 use crate::state::{config, config_read, State};
 use cosmwasm_std::{
     to_binary, Api, Binary, Env, Extern, HandleResponse, HumanAddr, InitResponse, Querier,
-    StdError, StdResult, Storage, Uint128,
+    StdError, StdResult, Storage,
 };
 use secret_toolkit::snip20;
 
@@ -18,7 +18,6 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
         admin: env.message.sender,
         contract_address: env.contract.address,
         viewing_key: msg.viewing_key.clone(),
-        withdrawal_allowed_from: msg.withdrawal_allowed_from,
     };
 
     config(&mut deps.storage).save(&state)?;
@@ -54,10 +53,6 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
 ) -> StdResult<HandleResponse> {
     match msg {
         HandleMsg::ChangeAdmin { address, .. } => change_admin(deps, env, address),
-        HandleMsg::Receive {
-            from, amount, msg, ..
-        } => receive(deps, env, from, amount, msg),
-        HandleMsg::Withdraw {} => withdraw(deps, env),
     }
 }
 
@@ -116,73 +111,12 @@ fn public_config<S: Storage, A: Api, Q: Querier>(
     Ok(ConfigResponse {
         accepted_token: state.accepted_token,
         admin: state.admin,
-        withdrawal_allowed_from: state.withdrawal_allowed_from,
-    })
-}
-
-fn receive<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    _from: HumanAddr,
-    _amount: Uint128,
-    _msg: Binary,
-) -> StdResult<HandleResponse> {
-    // Ensure that the sent tokens are from an expected contract address
-    let state = config_read(&deps.storage).load()?;
-    if env.message.sender != state.accepted_token.address {
-        return Err(StdError::generic_err(format!(
-            "This token is not supported. Supported: {}, given: {}",
-            state.accepted_token.address, env.message.sender
-        )));
-    }
-
-    Ok(HandleResponse {
-        messages: vec![],
-        log: vec![],
-        data: None,
-    })
-}
-
-fn withdraw<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-) -> StdResult<HandleResponse> {
-    let state = config_read(&deps.storage).load()?;
-    // Ensure that admin is calling this
-    if env.message.sender != state.admin {
-        return Err(StdError::Unauthorized { backtrace: None });
-    }
-
-    // Ensure that the current time is after the withdrawal_allowed_from
-    if env.block.time < state.withdrawal_allowed_from {
-        return Err(StdError::generic_err(format!(
-            "Withdrawal not allowed yet. Withdrawal allowed from: {}, current time: {}",
-            state.withdrawal_allowed_from, env.block.time
-        )));
-    }
-
-    let amount_of_accepted_token_to_send = accepted_token_available(deps)?.amount;
-    // Transfer accepted token to admin
-    let messages = vec![snip20::transfer_msg(
-        state.admin,
-        amount_of_accepted_token_to_send,
-        None,
-        RESPONSE_BLOCK_SIZE,
-        state.accepted_token.contract_hash,
-        state.accepted_token.address,
-    )?];
-
-    Ok(HandleResponse {
-        messages,
-        log: vec![],
-        data: None,
     })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::msg::ReceiveMsg;
     use crate::state::SecretContract;
     use cosmwasm_std::from_binary;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage};
@@ -205,7 +139,6 @@ mod tests {
         let msg = InitMsg {
             accepted_token: accepted_token.clone(),
             viewing_key: "nannofromthegirlfromnowhereisathaidemon?".to_string(),
-            withdrawal_allowed_from: 3,
         };
         (init(&mut deps, env.clone(), msg), deps)
     }
@@ -252,87 +185,8 @@ mod tests {
             ConfigResponse {
                 accepted_token: accepted_token,
                 admin: HumanAddr::from(MOCK_ADMIN),
-                withdrawal_allowed_from: 3
             },
             value
-        );
-    }
-
-    #[test]
-    fn test_receive_accepted_token_callback() {
-        let (_init_result, mut deps) = init_helper();
-        let amount: Uint128 = Uint128(333);
-        let from: HumanAddr = HumanAddr::from("someuser");
-
-        // Test that only accepted token is accepted
-        // Not accepted token
-        let msg = HandleMsg::Receive {
-            amount: amount,
-            from: from.clone(),
-            sender: from.clone(),
-            msg: to_binary(&ReceiveMsg::Deposit {}).unwrap(),
-        };
-        let handle_response = handle(&mut deps, mock_env("notasupportedtoken", &[]), msg.clone());
-        assert_eq!(
-            handle_response.unwrap_err(),
-            StdError::GenericErr {
-                msg: format!(
-                    "This token is not supported. Supported: {}, given: {}",
-                    MOCK_ACCEPTED_TOKEN_ADDRESS, "notasupportedtoken"
-                ),
-                backtrace: None
-            }
-        );
-
-        // Accepted token
-        let msg = HandleMsg::Receive {
-            amount: amount,
-            from: from.clone(),
-            sender: from,
-            msg: to_binary(&ReceiveMsg::Deposit {}).unwrap(),
-        };
-        let handle_response = handle(
-            &mut deps,
-            mock_env(MOCK_ACCEPTED_TOKEN_ADDRESS, &[]),
-            msg.clone(),
-        );
-        handle_response.unwrap();
-    }
-
-    #[test]
-    fn test_withdraw() {
-        let (_init_result, mut deps) = init_helper();
-        let msg = HandleMsg::Withdraw {};
-        let env_block_time = mock_env("anyone", &[]).block.time;
-
-        // It raises an error when non-admin calls it
-        let handle_response = handle(&mut deps, mock_env("anyone", &[]), msg.clone());
-        assert_eq!(
-            handle_response.unwrap_err(),
-            StdError::Unauthorized { backtrace: None }
-        );
-
-        // It raises an error when admin calls this too early
-        let mut state = config_read(&deps.storage).load().unwrap();
-        state.withdrawal_allowed_from = env_block_time + 3;
-        config(&mut deps.storage).save(&state).unwrap();
-        let handle_response = handle(&mut deps, mock_env(MOCK_ADMIN, &[]), msg.clone());
-        assert_eq!(
-            handle_response.unwrap_err(),
-            StdError::generic_err(format!(
-                "Withdrawal not allowed yet. Withdrawal allowed from: {}, current time: {}",
-                state.withdrawal_allowed_from, env_block_time
-            )),
-        );
-
-        // Test that a request is sent to the offered token contract address to transfer tokens to admin
-        let mut state = config_read(&deps.storage).load().unwrap();
-        state.withdrawal_allowed_from = env_block_time - 3;
-        config(&mut deps.storage).save(&state).unwrap();
-        let handle_response = handle(&mut deps, mock_env(MOCK_ADMIN, &[]), msg.clone());
-        assert_eq!(
-            handle_response.unwrap_err(),
-            StdError::generic_err(format!("Error performing Balance query: Generic error: Querier system error: No such contract: {}", MOCK_ACCEPTED_TOKEN_ADDRESS)),
         );
     }
 }
