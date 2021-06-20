de8cc595ff5aadc7605d6fe554bc2e626fa8cf82
Steven Chang <steven.chang@btn.group>
2021-06-20 14:42:52 +1000

    [BG-317] Mint user pool share tokens when contract receives accepted_token (#4)

--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,5 @@
 use crate::state::SecretContract;
-use cosmwasm_std::{HumanAddr, Uint128};
+use cosmwasm_std::{Binary, HumanAddr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -17,6 +17,12 @@ pub enum HandleMsg {
         address: HumanAddr,
         padding: Option<String>,
     },
+    Receive {
+        sender: HumanAddr,
+        from: HumanAddr,
+        amount: Uint128,
+        msg: Binary,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -24,6 +30,12 @@ pub enum HandleMsg {
 pub enum QueryMsg {
     AcceptedTokenAvailable {},
     Config {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ReceiveMsg {
+    Deposit {},
 }
 
 // QUERY RESPONSE STRUCTS
